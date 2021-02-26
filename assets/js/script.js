e5ae53bfb8b43e5264cd2a224027bf9331d5369d
Mahir Xosrovoğlu <61503554+MahirMammadlee@users.noreply.github.com>
2021-02-26 13:53:38 +0400

    .
    .

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function javaHover() {
+  let element = document.querySelector("#javaOverlay");
+  element.style.display = "block";
+}
+function javaOut() {
+  let element = document.querySelector("#javaOverlay");
+  element.style.display = "none";
+}
+
+function astroHover() {
+  let element = document.querySelector("#astroOverlay");
+  element.style.display = "block";
+}
+function astroOut() {
+  let element = document.querySelector("#astroOverlay");
+  element.style.display = "none";
+}
+
+function blogHover() {
+  let element = document.querySelector("#blogOverlay");
+  element.style.display = "block";
+}
+function blogOut() {
+  let element = document.querySelector("#blogOverlay");
+  element.style.display = "none";
+}
+
